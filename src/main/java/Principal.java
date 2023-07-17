ff9600f5ef181fda7df2ec783e6a98b61b82c9b0
mateus-damasceno-zup <mateus.damasceno@zup.com.br>
2023-07-16 22:34:29 -0300

    teste crud rebelde

---
+++ src/main/java/Principal.java
@@ -0,0 +1,11 @@
+import connection.Conexao;
+import view.RebeldeView;
+
+public class Principal {
+    public static void main(String[] args) {
+        RebeldeView rebeldeView= new RebeldeView();
+        rebeldeView.incializacao();
+
+
+    }
+}
